c9bc1c5a57251c1f2a542f71de9064a0641ff6c6
47486245+Edmartt@users.noreply.github.com <47486245+Edmartt@users.noreply.github.com>
2026-05-08 17:19:25 -0500

    feat: adds auth0 validation for endpoints authortization

--- internal/application/server.go
+++ internal/application/server.go
@@ -1,8 +1,12 @@
 package application
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/auth0/go-jwt-middleware/v3/validator"
+	"github.com/edmartt/bookstatic-book-service/internal/config"
+	"github.com/edmartt/bookstatic-book-service/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,10 +20,12 @@ func (h HTTPServer) setBookRoutes(router *gin.RouterGroup) {
 	router.PATCH("/books/:id", h.Handler.UpdateBook)
 }
 
-func (h HTTPServer) setRouter() *gin.Engine {
+func (h HTTPServer) setRouter(validatorJWT *validator.Validator) *gin.Engine {
 	router := gin.Default()
 
 	router.Use(LimitRequest())
+
+	router.Use(AuthMiddleware(validatorJWT))
 	apiGroup := router.Group("/api/v1")
 	h.setBookRoutes(apiGroup)
 
@@ -28,7 +34,19 @@ func (h HTTPServer) setRouter() *gin.Engine {
 
 // RunServer starts http server
 func (h HTTPServer) RunServer(port string) error {
-	router := h.setRouter()
+	auth0ConfigObject, err := config.LoadAuthConfig()
+
+	if err != nil {
+		return fmt.Errorf("error loading AUTH0 Config: %w", err)
+	}
+
+	jwtValidator, err := utils.JWTvalidator(auth0ConfigObject.Domain, auth0ConfigObject.Audience)
+
+	if err != nil {
+		return fmt.Errorf("error creating validator: %w", err)
+	}
+
+	router := h.setRouter(jwtValidator)
 	log.Fatal(router.Run(":" + port))
 
 	return nil
