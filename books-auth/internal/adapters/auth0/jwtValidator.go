a23e8ff6dbdccb486d716fa3815459e81394fcc0
47486245+Edmartt@users.noreply.github.com <47486245+Edmartt@users.noreply.github.com>
2026-05-25 22:44:28 -0500

    refactor: refactor jwt validator for making possible other identity providers like Cognito

--- books-auth/internal/adapters/auth0/jwtValidator.go
+++ books-auth/internal/adapters/auth0/jwtValidator.go
@@ -1,47 +1,45 @@
 package auth0
 
 import (
-	"fmt"
+	"context"
 	"net/url"
-	"os"
 	"time"
 
 	"github.com/auth0/go-jwt-middleware/v3/jwks"
 	"github.com/auth0/go-jwt-middleware/v3/validator"
+	projectDomain "github.com/edmartt/booktastic-auth-service/internal/core/domain"
 )
 
-func JWTvalidator(domain, audience string) (*validator.Validator, error) {
-	issuerURL, err := url.Parse(os.Getenv("AUTH0_DOMAIN"))
+type Auth0TokenValidator struct {
+	localValidator *validator.Validator
+}
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse issuer URL")
-	}
-	provider, err := jwks.NewCachingProvider(
-
+func NewAuth0TokenValidator(domain, audience string) (*Auth0TokenValidator, error) {
+	issuerURL, _ := url.Parse(domain)
+	provider, _ := jwks.NewCachingProvider(
 		jwks.WithIssuerURL(issuerURL),
 		jwks.WithCacheTTL(5*time.Minute),
 	)
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to create JWKS provider: %w", err)
-	}
-
-	jwtValidator, err := validator.New(
+	jwtValidator, _ := validator.New(
 
 		validator.WithKeyFunc(provider.KeyFunc),
 		validator.WithAlgorithm(validator.RS256),
 		validator.WithIssuer(issuerURL.String()),
 		validator.WithAudience(audience),
 		validator.WithCustomClaims(func() validator.CustomClaims {
-			return &CustomClaims{}
+
+			return &projectDomain.CustomClaims{}
 		}),
 		validator.WithAllowedClockSkew(30*time.Second),
 	)
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to create validator: %w", err)
-	}
+	return &Auth0TokenValidator{
+		localValidator: jwtValidator,
+	}, nil
+}
 
-	return jwtValidator, nil
+func (v *Auth0TokenValidator) Validate(ctx context.Context, token string) (any, error) {
 
+	return v.localValidator.ValidateToken(ctx, token)
 }
